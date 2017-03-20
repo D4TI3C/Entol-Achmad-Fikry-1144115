2e4afe23ed1c91207584997da34b69155b479e61
enfikry25 <entolfikry95@gmail.com>
2017-03-21 00:05:10 +0700

    pertemuan2

---
+++ doc/aplikasi/tugas2.py
@@ -0,0 +1,29 @@
+rute = {
+	'cileunyi': ['cibiru'], 
+	'cibiru': ['ujungberung'], 
+	'ujungberung': ['cicaheum'], 
+	'cicaheum': ['dago'], 
+	'dago': ['pasteur'], 
+	'pasteur': ['cileunyi']
+	}
+
+def rute_perjalanan(rute, ruteawal, ruteakhir, jalan=[]):
+        jalan = jalan + [ruteawal]
+        if ruteawal == ruteakhir:
+            return jalan
+	    if not rute.has_key(ruteawal):
+            	return None
+        rutejalan = None
+        for x in rute[ruteawal]:
+            if x not in jalan:
+                rutebaru = rute_perjalanan(rute, x, ruteakhir, jalan)
+                if rutebaru:
+                    if not rutejalan or len(rutebaru) < len(rutejalan):
+                        rutejalan = rutebaru
+        return rutejalan
+
+print("Rute :\n - cileunyi \n - cibiru \n - ujungberung \n - cicaheum \n - dago \n - pasteur \n")
+ruteawal = raw_input("Masukkan Rute Awal : ")
+ruteakhir = raw_input("Masukkan Rute AKhir : ")
+jalur = rute_perjalanan(rute, ruteawal, ruteakhir, jalan=[])
+print 'Rute Perjalanan :', jalur
